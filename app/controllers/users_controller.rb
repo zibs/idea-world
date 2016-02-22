666021d5739ab42ae0fad93327865ad84463e771
zibs <elizibin@gmail.com>
2016-02-22 13:58:25 -0800

    allow editing of users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+before_action :find_user, only: [:edit, :update, :show, :destroy]
 
   def new
     @user = User.new
@@ -16,7 +17,24 @@ class UsersController < ApplicationController
     end
   end
 
+    def edit
+    end
+
+    def update
+      if @user.update(user_params)
+        redirect_to root_path, flash: { success: "Update Successful" }
+      else
+        render :edit
+      end
+    end
+
+
     private
+
+
+      def find_user
+        @user = User.find(params[:id])
+      end
 
       def user_params
         params.require(:user).permit([:first_name, :last_name, :email, :password, :password_confirmation])
